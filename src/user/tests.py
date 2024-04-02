bab8ce7855aabd633fb5525e2b7391dec7d68526
Sergey <ssnovak@stud.kpfu.ru>
2024-04-02 19:41:11 +0300

    + test

--- src/user/tests.py
+++ src/user/tests.py
@@ -1,9 +1,11 @@
 import datetime
+import json
 import jwt
 
 from django.conf import settings
 from django.contrib.auth import get_user_model
-from rest_framework.test import APITestCase
+from django.test import TestCase
+from rest_framework.test import APITestCase, APIClient
 
 User = get_user_model()
 
@@ -57,4 +59,59 @@ class CreateUserAPIViewTests(APITestCase):
     def test_create_user_invalid_data(self):
         invalid_data = {'first_name': 'testuser', 'password': 'password'}
         response = self.client.post('/user/create/', invalid_data, format='json')
+        self.assertEqual(response.status_code, 400)
+
+
+class PasswordChangeServiceTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(first_name='test_user', email='test@example.com', password='old_password')
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_password_change_incorrect_old_password(self):
+        data = {'old_password': 'wrong_password', 'new_password': 'new_password'}
+        response = self.client.post(
+            '/user/password/change/', data=json.dumps(data), content_type='application/json'
+        )
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data['error'], 'Incorrect old password.')
+        self.user.refresh_from_db()
+        self.assertTrue(self.user.check_password('old_password'))
+
+    def test_password_change_success(self):
+        data = {'old_password': 'old_password', 'new_password': 'new_password'}
+        response = self.client.post(
+            '/user/password/change/', data=json.dumps(data), content_type='application/json'
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['message'], 'Password changed successfully.')
+        self.user.refresh_from_db()
+        self.assertTrue(self.user.check_password('new_password'))
+
+
+class PasswordResetServiceTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(first_name='test_user', email='test@example.com', password='old_password')
+        self.client = APIClient()
+
+    def test_password_forgive_success(self):
+        data = {'email': 'test@example.com'}
+        response = self.client.post(
+            '/user/password/forgive/', data=json.dumps(data), content_type='application/json'
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['message'], 'Password changed successfully.')
+
+    def test_password_forgive_invalid_email(self):
+        data = {'email': 'nonexistent@example.com'}
+        response = self.client.post(
+            '/user/password/forgive/', data=json.dumps(data), content_type='application/json'
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_password_reset_invalid_code(self):
+        data = {'email': 'test@example.com', 'recover_code': 'invalid_code', 'new_password': 'new_password'}
+        response = self.client.post(
+            '/user/password/reset/', data=json.dumps(data), content_type='application/json'
+        )
         self.assertEqual(response.status_code, 400)
